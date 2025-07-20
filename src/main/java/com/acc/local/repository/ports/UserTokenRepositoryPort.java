abf02dc5e2946307d783354041a4f6fafac0dce6
hyunje <guswp320@ajou.ac.kr>
2025-07-20 23:18:52 +0900

    feat(UserToken) : UserToken repository 계층 생성 - 포트,  어댑터, jpa 모듈 생성

---
+++ src/main/java/com/acc/local/repository/ports/UserTokenRepositoryPort.java
@@ -0,0 +1,21 @@
+package com.acc.local.repository.ports;
+
+import com.acc.local.entity.UserTokenEntity;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface UserTokenRepositoryPort {
+    
+    UserTokenEntity save(UserTokenEntity userToken);
+    
+    Optional<UserTokenEntity> findByJwtTokenAndIsActiveTrue(String jwtToken);
+    
+    Optional<UserTokenEntity> findByUserIdAndIsActiveTrue(String userId);
+    
+    void deactivateAllByUserId(String userId);
+    
+    void deactivateExpiredTokens();
+    
+    void deleteInactiveTokens();
+}
