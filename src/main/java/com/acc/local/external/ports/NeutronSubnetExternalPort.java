b8408dd0e0196ce3101f0e58f21a188a4fec1e38
천 진강 <jjjjjk12@ajou.ac.kr>
2025-11-22 10:56:45 +0900

    [Fix] Default Router 누락 문제 해결 #59

--- src/main/java/com/acc/local/external/ports/NeutronSubnetExternalPort.java
+++ src/main/java/com/acc/local/external/ports/NeutronSubnetExternalPort.java
@@ -3,8 +3,9 @@ package com.acc.local.external.ports;
 import com.acc.local.dto.network.CreateNetworkRequest;
 
 import java.util.List;
+import java.util.Map;
 
 public interface NeutronSubnetExternalPort {
 
-    void callCreateSubnet(String keystoneToken, List<CreateNetworkRequest.Subnet> subnets, String networkId);
+    List<Map<String, String>> callCreateSubnet(String keystoneToken, List<CreateNetworkRequest.Subnet> subnets, String networkId);
 }
