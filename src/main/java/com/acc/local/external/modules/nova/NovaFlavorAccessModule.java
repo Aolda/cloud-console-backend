a91a3005cf216fdad732d030be159714ccbf06bd
휘래 김 <hramst0618@gmail.com>
2025-11-23 02:21:04 +0900

    feat: openstack nova 최신 버전 사용

--- src/main/java/com/acc/local/external/modules/nova/NovaFlavorAccessModule.java
+++ src/main/java/com/acc/local/external/modules/nova/NovaFlavorAccessModule.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -18,6 +20,13 @@ public class NovaFlavorAccessModule extends NovaAPIUtil {
     // Tenant Access 추가 (Private Flavor용)
     public ResponseEntity<JsonNode> addTenantAccess(String token, String flavorId, FlavorAccessRequest request) {
         String uri = "/v2.1/flavors/" + flavorId + "/action";
-        return openstackAPICallModule.callPostAPI(uri, Collections.singletonMap("X-Auth-Token", token), request, port);
+        return openstackAPICallModule.callPostAPI(uri, createHeaders(token), request, port);
+    }
+
+    private Map<String, String> createHeaders(String token) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("X-Auth-Token", token);
+        headers.put("X-OpenStack-Nova-API-Version", "2.79");
+        return headers;
     }
 }
--- src/main/java/com/acc/local/external/modules/nova/NovaFlavorExtraSpecsModule.java
+++ src/main/java/com/acc/local/external/modules/nova/NovaFlavorExtraSpecsModule.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -18,6 +20,13 @@ public class NovaFlavorExtraSpecsModule extends NovaAPIUtil {
     // Extra Specs 생성
     public ResponseEntity<JsonNode> createExtraSpecs(String token, String flavorId, FlavorExtraSpecsRequest request) {
         String uri = "/v2.1/flavors/" + flavorId + "/os-extra_specs";
-        return openstackAPICallModule.callPostAPI(uri, Collections.singletonMap("X-Auth-Token", token), request, port);
+        return openstackAPICallModule.callPostAPI(uri, createHeaders(token), request, port);
+    }
+
+    private Map<String, String> createHeaders(String token) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("X-Auth-Token", token);
+        headers.put("X-OpenStack-Nova-API-Version", "2.79");
+        return headers;
     }
 }
