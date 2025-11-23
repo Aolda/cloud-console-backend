f788c449cf4f7536baeba0899a895d881cc631d6
minjoonJ <breauxchristoper@gmail.com>
2025-11-23 22:37:38 +0900

    feat: error code

--- src/main/java/com/acc/global/exception/image/ImageErrorCode.java
+++ src/main/java/com/acc/global/exception/image/ImageErrorCode.java
@@ -13,7 +13,10 @@ public enum ImageErrorCode implements ErrorCode {
     IMAGE_DELETE_FAILURE(502, "ACC-IMAGE-DELETE-FAILURE", "이미지 삭제 중 오류가 발생하였습니다."),
     INVALID_IMAGE_METADATA(400, "ACC-IMAGE-METADATA-INVALID", "이미지 메타데이터 형식이 올바르지 않습니다."),
     IMAGE_LIST_FETCH_FAILURE(502, "ACC-IMAGE-FETCH-LIST-FAILURE", "이미지 목록 조회 중 오류가 발생했습니다."),
-    IMAGE_FILE_UPLOAD_FAILURE(502, "ACC-IMAGE-UPLOAD-FAILURE", "이미지 파일 업로드 중 오류가 발생했습니다.");
+    IMAGE_FILE_UPLOAD_FAILURE(502, "ACC-IMAGE-UPLOAD-FAILURE", "이미지 파일 업로드 중 오류가 발생했습니다."),
+    INVALID_PAGINATION_PARAM(400, "ACC-IMAGE-PAGINATION-PARAM-INVALID", "잘못된 페이지네이션 파라미터 조합입니다."),
+    INVALID_PAGINATION_WITH_IMAGE_ID(400, "ACC-IMAGE-PAGINATION-WITH-IMAGE-ID", "이미지 단건 조회 시 페이지네이션 파라미터를 함께 사용할 수 없습니다.");
+
 
     private final int status;
     private final String code;
