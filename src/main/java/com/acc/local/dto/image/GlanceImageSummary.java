0462e0f6339f99774f699a8cfbcfeab286a77eae
minjoonJ <breauxchristoper@gmail.com>
2025-11-25 19:42:55 +0900

    feat: GlanceImageSummary

--- src/main/java/com/acc/local/dto/image/GlanceImageSummary.java
+++ src/main/java/com/acc/local/dto/image/GlanceImageSummary.java
@@ -2,27 +2,19 @@ package com.acc.local.dto.image;
 
 import lombok.Builder;
 
-import java.util.List;
-
 @Builder
-public record ImageListResponse(
-        List<GlanceImageSummary> images
-) {
-
-    @Builder
-    public record GlanceImageSummary(
-            String id,
-            String name,
-            String architecture,
-            String projectName,
-            String description,
-            String diskFormat,
-            String status,
-            String visibility,
-            Long size,
-            Boolean hidden,
-            Integer minDisk,
-            Integer minRam,
-            String createdAt
-    ) {}
-}
+public record GlanceImageSummary(
+        String id,
+        String name,
+        String architecture,
+        String projectName,
+        String description,
+        String diskFormat,
+        String status,
+        String visibility,
+        Long size,
+        Boolean hidden,
+        Integer minDisk,
+        Integer minRam,
+        String createdAt
+) {}
