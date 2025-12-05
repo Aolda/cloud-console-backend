8e420caf964d9aa6aa1a814fd3ae5f058fdfec97
nAhYUN22 <lily.nahyunlee@gmail.com>
2025-12-06 01:08:36 +0900

    fix: Volume, Snapshot, SnapshotPolicy Docs에 인증 요구사항 추가

--- src/main/java/com/acc/local/controller/docs/VolumeDocs.java
+++ src/main/java/com/acc/local/controller/docs/VolumeDocs.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Volume", description = "볼륨 API")
 @RequestMapping("/api/v1/volumes")
+@SecurityRequirement(name = "access-token")
 public interface VolumeDocs {
 
     @Operation(
