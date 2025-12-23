cf6792007cd671320fed57c8a82f1a704feb05e8
waffle <nate2402@ajou.ac.kr>
2025-12-23 13:05:35 +0900

    fix: quota 일부가 명시적으로 지정되지 않은 프로젝트에 대하여 특정조건 일치 시 quota가 출력되지 않는 문제 수정

--- src/main/java/com/acc/local/dto/project/quota/ProjectGlobalQuotaDto.java
+++ src/main/java/com/acc/local/dto/project/quota/ProjectGlobalQuotaDto.java
@@ -2,32 +2,38 @@ package com.acc.local.dto.project.quota;
 
 import lombok.Builder;
 
-@Builder
 public record ProjectGlobalQuotaDto(
 	QuotaInformation instance,
 	QuotaInformation core,
 	QuotaInformation ram,
 	QuotaGroup volume
 ) {
-	public static ProjectGlobalQuotaDto getDefault() {
-		return ProjectGlobalQuotaDto.builder()
-			.core(QuotaInformation.builder()
-				.available(8)
-				.build())
-			.ram(QuotaInformation.builder()
-				.available(32)
-				.build())
-			.instance(QuotaInformation.builder()
-				.available(10)
-				.build())
-			.volume(QuotaGroup.builder()
-				.count(QuotaInformation.builder()
-					.available(10)
-					.build())
-				.size(QuotaInformation.builder()
-					.available(1000)
-					.build())
-				.build())
+
+	private static final QuotaInformation baseCoreQuota = QuotaInformation.builder().available(8).build();
+	private static final QuotaInformation baseRamQuota = QuotaInformation.builder().available(32).build();
+	private static final QuotaInformation baseInstanceQuota = QuotaInformation.builder().available(10).build();
+	private static final QuotaGroup baseVolumeQuotaGroup = QuotaGroup.builder()
+		.count(QuotaInformation.builder().available(10).build())
+		.size(QuotaInformation.builder().available(1000).build())
 		.build();
+
+	@Builder
+	public ProjectGlobalQuotaDto {
+		if (instance == null) {
+			instance = baseCoreQuota;
+		}
+		if (core == null) {
+			core = baseRamQuota;
+		}
+		if (ram == null) {
+			ram = baseInstanceQuota;
+		}
+		if (volume == null) {
+			volume = baseVolumeQuotaGroup;
+		}
+	}
+
+	public static ProjectGlobalQuotaDto getDefault() {
+		return ProjectGlobalQuotaDto.builder().build();
 	}
 }
