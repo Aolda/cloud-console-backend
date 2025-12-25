4298b0265eef644c7cdb89e850d69d9bf3310413
천 진강 <jjjjjk12@ajou.ac.kr>
2025-12-25 16:43:24 +0900

    feat: 로깅 인터셉터 작성 및 등록

--- src/main/java/com/acc/global/config/WebMvcConfig.java
+++ src/main/java/com/acc/global/config/WebMvcConfig.java
@@ -1,4 +1,20 @@
 package com.acc.global.config;
 
-public class WebMvcConfig {
+import com.acc.global.logging.DomainLoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final DomainLoggingInterceptor domainLoggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(domainLoggingInterceptor)
+                .addPathPatterns("/**");
+    }
 }
--- src/main/java/com/acc/global/logging/DomainLoggingInterceptor.java
+++ src/main/java/com/acc/global/logging/DomainLoggingInterceptor.java
@@ -1,4 +1,28 @@
 package com.acc.global.logging;
 
-public class DomainLogginInterceptor {
+import com.acc.global.annotation.LogDomain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class DomainLoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            LogDomain logDomain = handlerMethod.getBeanType().getAnnotation(LogDomain.class);
+            if (logDomain != null) {
+                MDC.put("domain", logDomain.value());
+            } else {
+                MDC.put("domain", "common");
+            }
+        }
+
+        return true;
+    }
 }
