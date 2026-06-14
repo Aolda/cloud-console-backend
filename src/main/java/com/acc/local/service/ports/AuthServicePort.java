8270bb94cccf2a4cd62dae265a60c2b695966215
이현제 <guswp320@ajou.ac.kr>
2026-06-14 16:46:10 +0900

    Remove: 레거시 Auth API 엔드포인트 제거

--- src/main/java/com/acc/local/service/ports/AuthServicePort.java
+++ src/main/java/com/acc/local/service/ports/AuthServicePort.java
@@ -1,23 +1,7 @@
 package com.acc.local.service.ports;
 
-import com.acc.local.domain.enums.project.ProjectRole;
-import com.acc.local.dto.auth.*;
-import com.acc.local.dto.project.UserPermissionResponse;
+import com.acc.local.dto.auth.LoginedUserProfileResponse;
 
 public interface AuthServicePort {
-    String authenticateAndGenerateJwt(String keycloakToken);
-    ProjectRole getProjectPermission(String ProjectId , String userId);
-    UserPermissionResponse getUserPermission(String keystoneProjectId, String userId);
-    boolean validateJwt(String jwtToken);
-    void invalidateUserTokens(String userId);
-    GetUserResponse getUserDetail(String targetUserId, String requesterId);
-
-    String authenticateKeystoneAndGenerateJwt(KeystonePasswordLoginRequest request);
-    LoginTokens login(KeystonePasswordLoginRequest request);
-    LoginTokens refreshToken(String refreshToken);
-    SignupResponse signup(SignupRequest request, String verificationToken);
-
 	LoginedUserProfileResponse getUserLoginedProfile(String userId, String projectId);
-
-	void logout(String userId);
 }
